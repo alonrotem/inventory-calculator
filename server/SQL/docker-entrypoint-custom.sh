ad3738c421f4067b11af38b279f8f1b64ad3941d
Alon Rotem <alrotem@gmail.com>
2026-08-27 22:04:36 +0300

    Docker runtime fixes

--- server/SQL/docker-entrypoint-custom.sh
+++ server/SQL/docker-entrypoint-custom.sh
@@ -4,15 +4,16 @@ set -e
 # Start MySQL in the background (using the official entrypoint)
 /usr/local/bin/docker-entrypoint.sh mysqld &
 
-# Wait for MySQL to be ready
-until mysqladmin ping -h "localhost" --silent; do
+# Wait for MySQL to be ready by passing the root password
+until mysqladmin ping -h "127.0.0.1" -u root -p"$MYSQL_ROOT_PASSWORD" --silent; do
+  echo "Waiting for MySQL to start..."
   sleep 1
 done
 
 # Run migrations every time container starts
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /db/01_create_db.sql
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /db/02_migration_functions.sql
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /db/03_migrations.sql
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /db/01_create_db.sql
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /db/02_migration_functions.sql
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /db/03_migrations.sql
 
 # Bring MySQL to the foreground
 wait
